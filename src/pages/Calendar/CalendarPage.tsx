aa9d92272791b44c310e018570711aa7b2856649
dayoung0311 <dyl0311@gachon.ac.kr>
2026-03-06 14:38:21 +0900

    캘린더 라이브러리 적용

--- src/pages/Calendar/CalendarPage.tsx
+++ src/pages/Calendar/CalendarPage.tsx
@@ -1,7 +1,28 @@
-function CalendarPage(){
-    return(
+import FullCalendar from "@fullcalendar/react"
+import dayGridPlugin from "@fullcalendar/daygrid";
+
+const mockEvents = [
+  {
+    id: "1",
+    title: "SQLD 제58회",
+    date: "2026-04-20",
+  },
+  {
+    id: "2",
+    title: "토익 시험",
+    date: "2026-04-25",
+  },
+];
+
+function CalendarPage() {
+    return (
         <div>
-            SchedulePage 
+            <FullCalendar
+                plugins={[dayGridPlugin]}
+                initialView="dayGridMonth"
+                events={mockEvents}
+                height="auto"
+            />
         </div>
     )
 }
