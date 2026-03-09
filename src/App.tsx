bba6096badbb09f8685ec5e23fe68c17515e33bb
dayoung0311 <dyl0311@gachon.ac.kr>
2026-03-09 11:14:46 +0900

    푸터 CSS 수정

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,29 @@ import Header from './components/layout/Header/Header'
 import CertManage from './pages/CertManage/CertManage'
 import CertSearch from './pages/CertSearch/CertSearch'
 import MyPage from './pages/MyPage/MyPage'
+import Footer from './components/layout/Footer/Footer'
 
 function App() {
 
   return (
-    <div>
+    <div className={styles.container}>
       <div className={styles.header}>
-      <Header/>
+        <Header />
       </div>
 
-      <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/calendar" element={<SchedulePage/>}/>
-        <Route path="/cert-manage" element={<CertManage/>}/>
-        <Route path="/cert-search" element={<CertSearch/>}/>
-        <Route path="/mypage" element={<MyPage/>}/>
-      </Routes>
+      <main className={styles.main}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/calendar" element={<SchedulePage />} />
+          <Route path="/cert-manage" element={<CertManage />} />
+          <Route path="/cert-search" element={<CertSearch />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
+      </main>
+
+      <div className={styles.footer}>
+        <Footer />
+      </div>
     </div>
   )
 }
---
+++ src/components/layout/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer(){
+    return(
+        <div className={styles.footer}>
+            푸터
+        </div>
+    )
+}
+
+export default Footer
