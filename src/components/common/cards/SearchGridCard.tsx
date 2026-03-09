7ebb9eda6681224a73563724bcbd3508306de04a
dayoung0311 <dyl0311@gachon.ac.kr>
2026-03-09 14:21:03 +0900

    SearchGridCard 제작

---
+++ src/components/common/cards/SearchGridCard.tsx
@@ -0,0 +1,48 @@
+interface SearchGridCardProps {
+  title: string;
+  description: string;
+}
+
+function SearchGridCard({ title, description }: SearchGridCardProps) {
+  return (
+    <div className="w-[400px] bg-white border border-gray-200 rounded-2xl shadow-sm hover:shadow-md transition overflow-hidden">
+
+      {/* 상단 영역 */}
+      <div className="p-6 flex items-start justify-between">
+        <div className="w-14 h-14 bg-gray-200 rounded-lg flex items-center justify-center text-xs font-bold">
+          AWS
+        </div>
+
+        <span className="text-blue-600 bg-blue-50 px-3 py-1 rounded-full text-sm font-medium">
+          전문가
+        </span>
+      </div>
+
+      {/* 제목 + 설명 */}
+      <div className="h-[240px] px-6 pb-6 flex flex-col gap-3">
+        <h3 className="text-2xl font-bold text-gray-900 leading-tight">
+          {title}
+        </h3>
+
+        <p className="text-gray-500 text-base pb-10 leading-relaxed">
+          {description}
+        </p>
+      </div>
+
+      {/* 하단 버튼 영역 */}
+      <div className="border-t bg-gray-50 border-gray-100 p-6 flex items-center justify-between">
+
+        <button className="flex items-center gap-2 text-green-800 font-medium">
+          일정 보기
+        </button>
+
+        <button className="bg-green-100 text-green-900 px-5 py-2 rounded-full font-medium hover:bg-green-200 transition">
+          내 자격증에 추가
+        </button>
+
+      </div>
+    </div>
+  );
+}
+
+export default SearchGridCard;
--- src/pages/CertSearch/CertSearch.tsx
+++ src/pages/CertSearch/CertSearch.tsx
@@ -1,9 +1,15 @@
-import styles from './CertSearch.module.css'
+import SearchGridCard from "../../components/common/cards/SearchGridCard"
 
-function CertSearch(){
-    return(
-        <div>
-            탐색페이지
+
+function CertSearch() {
+    return (
+        <div className="p-8">
+            <h1 className="font-semibold text-4xl pb-2">자격증 탐색</h1>
+            <p className="pb-5">커리어를 한 단계 높여줄 최적의 자격증을 리스트에서 확인해보세요.</p>
+            <SearchGridCard
+                title="AWS Certified Solutions Architect"
+                description="AWS 기술을 활용한 클라우드 설계 자격증"
+            />
         </div>
     )
 }
