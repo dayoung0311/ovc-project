80a1a9df9614ed46b3382ea40845e548c590dbe0
dayoung0311 <dyl0311@gachon.ac.kr>
2026-03-10 10:15:54 +0900

    type error solved

--- src/types/exam.ts
+++ src/types/exam.ts
@@ -11,7 +11,7 @@ export type Certificate = {
 
 export type Schedule = {
   scheduleId: number;
-  certificateName: number;
+  certificateName: string;
   examName: string;
   examType: string; //실기, 필기
   eventType: string; //지원일, 시험일, 결과일
@@ -20,18 +20,16 @@ export type Schedule = {
 };
 
 export type CalendarEventType = {
-  id: number;
+  id: string;
   title: string;
   start: string;
   end: string;
-  extended:extendedProps
-};
-
-export type extendedProps = {
-  scheduleId: number;
-    certificationName: string;
+  extendedProps: {
+    scheduleId: number;
+    certificateName: string;
     examType: string;
     eventType: string;
     startDate: string;
     endDate: string;
-}
+  };
+};
--- src/utils/calendar.ts
+++ src/utils/calendar.ts
@@ -1,10 +1,24 @@
 import type { Schedule } from "../types/exam";
+import type { CalendarEventType } from "../types/exam";
 
-export function mapSchedulesToEvents(schedules: Schedule[]) {
+export function mapSchedulesToEvents(
+  schedules: Schedule[]
+): CalendarEventType[] {
   return schedules.map((schedule) => ({
     id: String(schedule.scheduleId),
-    title: schedule.examName,
+
+    title: `${schedule.certificateName} ${schedule.eventType}`,
+
     start: schedule.startDate,
     end: schedule.endDate,
+
+    extendedProps: {
+      scheduleId: schedule.scheduleId,
+      certificateName: schedule.certificateName,
+      examType: schedule.examType,
+      eventType: schedule.eventType,
+      startDate: schedule.startDate,
+      endDate: schedule.endDate,
+    },
   }));
 }
