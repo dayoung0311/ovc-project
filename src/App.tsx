292f3a746c336565f3231bf2878b13a8e4c5b662
dayoung0311 <dyl0311@gachon.ac.kr>
2026-03-10 19:38:01 +0900

    라우터 파일 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { Route, Routes } from 'react-router-dom'
 import styles from './App.module.css'
-import HomePage from './pages/Home/HomePage'
-import SchedulePage from './pages/Calendar/CalendarPage'
 import Header from './components/layout/Header/Header'
-import CertManage from './pages/CertManage/CertManage'
-import CertSearch from './pages/CertSearch/CertSearch'
-import MyPage from './pages/MyPage/MyPage'
 import Footer from './components/layout/Footer/Footer'
-import LoginPage from './pages/Login/LoginPage'
-import SignupPage from './pages/Signup/SignupPage'
+import Router from './routes/Router'
 
 function App() {
 
@@ -19,15 +12,7 @@ function App() {
       </div>
 
       <main className={styles.main}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/calendar" element={<SchedulePage />} />
-          <Route path="/cert-manage" element={<CertManage />} />
-          <Route path="/cert-search" element={<CertSearch />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/login" element={<LoginPage/>} />
-          <Route path="/signup" element={<SignupPage />} />
-        </Routes>
+        <Router/>
       </main>
 
       <div className={styles.footer}>
