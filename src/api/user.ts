2a5d9f476f7f07b0b7f0c7e9da8caa8029f774a1
dayoung0311 <dyl0311@gachon.ac.kr>
2026-03-13 19:35:02 +0900

    rest API 경로 수정

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,6 +1,6 @@
 import { apiClient } from "./apiClient"
 
 export const getMyInfo= async () => {
-    const res=await apiClient.get("/users/me");
+    const res=await apiClient.get("/api/users/me");
     return res.data.data;
 }
