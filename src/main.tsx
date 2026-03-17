daa4fbab62fccd626f22f6d6c43035a21ad1d728
dayoung0311 <dyl0311@gachon.ac.kr>
2026-03-17 14:23:35 +0900

    페이지 변경 시 스크롤 위치 문제 해결

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import ScrollToTop from './components/scroll/ScrollToTop.tsx'
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+        <ScrollToTop />
         <App />
       </BrowserRouter>
     </QueryClientProvider>
